d08cbfdab07034caccc8e469f5d5b15b4ff2b6de
Elijah Moore <e.moore8902@gmail.com>
2022-06-01 11:04:29 -0700

    finished and tested all calculator pieces

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,42 @@
-const add = function() {
-	
+const add = function(x, y) {
+	return x+y;
 };
 
-const subtract = function() {
-	
+const subtract = function(x, y) {
+	return x-y;
 };
 
-const sum = function() {
-	
+const sum = function(array1) {
+const initialValue = 0;
+const sumWithInitial = array1.reduce(
+  (previousValue, currentValue) => previousValue + currentValue,
+  initialValue
+);
+return sumWithInitial;
 };
 
-const multiply = function() {
-
+const multiply = function(array1) {
+  let output = 1;
+  for (let i = 0; i < array1.length; i++){
+    output *= array1[i];
+  }
+  return output;
 };
 
-const power = function() {
-	
+const power = function(x,y) {
+	return Math.pow(x,y);
 };
 
-const factorial = function() {
-	
+const factorial = function(x) {
+	if (x<0){
+    return -1;
+  }
+  else if (x == 0){
+    return 1;
+  }
+  else {
+    return (x * factorial(x-1));
+  }
 };
 
 // Do not edit below this line
